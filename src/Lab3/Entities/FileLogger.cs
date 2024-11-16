17cd5ca94a38adc7acec3b65cc8fa9b91bfd78f1
shirr9 <simkagaechka1@gmail.com>
2024-11-16 13:13:33 +0300

    refactor: add file logger

---
+++ src/Lab3/Entities/FileLogger.cs
@@ -0,0 +1,18 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab3.Interfaces;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Entities;
+
+public class FileLogger : ILogger
+{
+    private readonly string _filePath;
+
+    public FileLogger(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public void Log(string message)
+    {
+        File.AppendAllText(_filePath, $"{DateTime.Now}:receive message {message}\n");
+    }
+}
