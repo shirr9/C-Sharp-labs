72e8b75c75435d64e50a8463185d3e19d88d99fa
shirr9 <simkagaechka1@gmail.com>
2024-11-16 13:16:43 +0300

    refactor: add decorators for messages

---
+++ src/Lab3/Entities/FilterMessagesDecorator.cs
@@ -0,0 +1,22 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab3.Interfaces;
+
+namespace Itmo.ObjectOrientedProgramming.Lab3.Entities;
+
+public class FilterMessagesDecorator : IDestination
+{
+    private readonly IDestination _destination;
+
+    private readonly int _importanceDestination;
+
+    public FilterMessagesDecorator(IDestination destination, int importanceDestination)
+    {
+        _destination = destination;
+        _importanceDestination = importanceDestination;
+    }
+
+    public void ReceiveMessage(IMessage message)
+    {
+        if (message.Importance < _importanceDestination) return;
+        _destination.ReceiveMessage(message);
+    }
+}
