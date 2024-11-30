2ce067ae4b8e687b6fff95a80e8eff361e85d75f
shirr9 <simkagaechka1@gmail.com>
2024-11-30 13:45:33 +0300

    refactor: add file systems

---
+++ src/Lab4/FileSystems/IFileSystem.cs
@@ -0,0 +1,22 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab4.FileSystems;
+
+public interface IFileSystem
+{
+    Guid Id { get; }
+
+    string Mode { get; }
+
+    string Address { get; set; }
+
+    void MoveFile(string sourcePath, string destinationPath);
+
+    void CopyFile(string sourcePath, string destinationPath);
+
+    void DeleteFile(string path);
+
+    void RenameFile(string path, string newName);
+
+    void GotoDirectory(string path);
+
+    void ConnectFileSystem(string address);
+}
