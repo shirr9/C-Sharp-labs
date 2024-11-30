e049bb5c59f4cf6a6d5337703ff1f65201f166f5
shirr9 <simkagaechka1@gmail.com>
2024-11-30 13:49:46 +0300

    refactor: add program system

---
+++ src/Lab4/Systems/IProgramSystem.cs
@@ -0,0 +1,15 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab4.FileSystems;
+using Itmo.ObjectOrientedProgramming.Lab4.Printer;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.Systems;
+
+public interface IProgramSystem
+{
+    IFileSystem? ConnectedFileSystem { get; }
+
+    void ConnectSystem(string address, string mode);
+
+    void DisconnectSystem();
+
+    IPrinter? TryFindPrinter(string mode);
+}
