95836fc8df484f139338661bcfb02a0d56801a44
shirr9 <simkagaechka1@gmail.com>
2024-12-20 18:57:07 +0300

    refactor: add models to domain

---
+++ src/Domain/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Models;
+
+public class Transaction
+{
+    public Transaction(DateTime timestamp, string type, int amount, int newBalance)
+    {
+        Timestamp = timestamp;
+        Type = type;
+        Amount = amount;
+        NewBalance = newBalance;
+    }
+
+    public DateTime Timestamp { get; }
+
+    public string Type { get; }
+
+    public int Amount { get; }
+
+    public int NewBalance { get; }
+}
