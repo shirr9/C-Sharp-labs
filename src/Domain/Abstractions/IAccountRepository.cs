8bc87ad8c78bc043fc8c08ec871a5a43ec8714e6
shirr9 <simkagaechka1@gmail.com>
2024-12-20 19:00:14 +0300

    refactor: add abstractions to domain

---
+++ src/Domain/Abstractions/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+
+namespace Domain.Abstractions;
+
+public interface IAccountRepository
+{
+    bool VerifyPin(int accountNumber, int pin);
+
+    Account GetAccount(int accountNumber);
+
+    void CreateAccount(Account account);
+
+    void UpdateAccount(Account account);
+}
---
+++ src/Domain/Abstractions/ITransactionRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+
+namespace Domain.Abstractions;
+
+public interface ITransactionRepository
+{
+    void AddTransaction(Transaction transaction, int accountNumber);
+
+    IEnumerable<Transaction> GetTransactions(int accountNumber);
+}
