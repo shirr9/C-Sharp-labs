b4251fd0999132e6681b19d4845797b9abd017c3
shirr9 <simkagaechka1@gmail.com>
2024-12-20 19:16:16 +0300

    refactor: add providers for user

---
+++ src/Presentation/User/UserLoginScenarioProvider.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Contracts;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Presentation.User;
+
+public class UserLoginScenarioProvider : IScenarioProvider
+{
+    private readonly IUserService _userService;
+    private readonly IAdminService _adminService;
+
+    public UserLoginScenarioProvider(IUserService userService, IAdminService adminService)
+    {
+        _userService = userService;
+        _adminService = adminService;
+    }
+
+    public bool TryGetScenario([NotNullWhen(true)] out IScenario? scenario)
+    {
+        if (_userService.IsLoggedIn() || _adminService.IsLoggedIn)
+        {
+            scenario = null;
+            return false;
+        }
+
+        scenario = new UserLoginScenario(_userService);
+        return true;
+    }
+}
---
+++ src/Presentation/User/UserOperationsScenarioProvider.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Contracts;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Presentation.User;
+
+public class UserOperationsScenarioProvider : IScenarioProvider
+{
+    private readonly IUserService _userService;
+
+    public UserOperationsScenarioProvider(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public bool TryGetScenario([NotNullWhen(true)] out IScenario? scenario)
+    {
+        if (!_userService.IsLoggedIn())
+        {
+            scenario = null;
+            return false;
+        }
+
+        var scenarios = new List<IScenario>
+        {
+            new GetBalanceScenario(_userService),
+            new WithdrawMoneyScenario(_userService),
+            new ReplenishAccountScenario(_userService),
+            new ViewTransactionHistoryScenario(_userService),
+        };
+
+        scenario = new CompositeScenario(scenarios);
+        return true;
+    }
+}
