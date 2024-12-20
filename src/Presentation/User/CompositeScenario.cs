ef5b6553ce2ca5299301b7b77442c1e01ae23a8d
shirr9 <simkagaechka1@gmail.com>
2024-12-20 19:16:48 +0300

    refactor: add composite scenario for user

---
+++ src/Presentation/User/CompositeScenario.cs
@@ -0,0 +1,27 @@
+﻿using Spectre.Console;
+
+namespace Presentation.User;
+
+public class CompositeScenario : IScenario
+{
+    public IEnumerable<IScenario> Scenarios { get; }
+
+    public CompositeScenario(IEnumerable<IScenario> scenarios)
+    {
+        Scenarios = scenarios;
+    }
+
+    public string Name => "Select action";
+
+    public void Run()
+    {
+        IScenario scenario = AnsiConsole.Prompt(
+            new SelectionPrompt<IScenario>()
+                .Title("[lightskyblue1]Select action[/]")
+                .HighlightStyle(new Style(foreground: new Color(135, 175, 0), decoration: Decoration.Bold))
+                .AddChoices(Scenarios)
+                .UseConverter(s => s.Name));
+
+        scenario.Run();
+    }
+}
